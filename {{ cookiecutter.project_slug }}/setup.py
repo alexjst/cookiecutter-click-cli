6f41c6463db9812a3f40454b8f3e6e6f68cc9934
Cody Hanson <cody@Codys-MacBook-Pro.local>
2020-01-24 18:16:17 -0600

    First commit

---
+++ {{ cookiecutter.project_slug }}/setup.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='{{ cookiecutter.project_name }}',
+    version='0.1.0',
+    include_package_data=True,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.command }}={{ cookiecutter.project_slug }}:cli',
+            ]
+        },
+    install_requires=[
+        'click',
+        #Add other requirements here
+    ]
+)
